ec6bdd0c27e27087d02955030da8d64bbd8d644c
Devina Lee Sheng tin <dleeshengtin@gmail.com>
2018-12-28 22:16:50 -0600

    completed working solution

--- src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -1,48 +1,159 @@
 ﻿using CheeseMVC.Models;
+
 using Microsoft.AspNetCore.Mvc.Rendering;
+
 using System;
+
 using System.Collections.Generic;
+
 using System.ComponentModel.DataAnnotations;
+
 using System.Linq;
+
 using System.Threading.Tasks;
 
+
+
 namespace CheeseMVC.ViewModels
+
 {
+
     public class AddCheeseViewModel
+
     {
+
         [Required]
+
         [Display(Name = "Cheese Name")]
+
         public string Name { get; set; }
 
+
+
         [Required(ErrorMessage = "You must give your cheese a description")]
+
         public string Description { get; set; }
 
-        public CheeseType Type { get; set; }
 
-        public List<SelectListItem> CheeseTypes { get; set; }
 
-        public AddCheeseViewModel() {
+        [Required]
 
-            CheeseTypes = new List<SelectListItem>();
+        [Display(Name = "Category")]
 
-            // <option value="0">Hard</option>
-            CheeseTypes.Add(new SelectListItem {
-                Value = ((int) CheeseType.Hard).ToString(),
-                Text = CheeseType.Hard.ToString()
-            });
+        public int CategoryID { get; set; }
 
-            CheeseTypes.Add(new SelectListItem
+
+
+        public List<SelectListItem> Categories { get; set; }
+
+
+
+        // necessary so model binding will work
+
+        public AddCheeseViewModel() { }
+
+
+
+        public AddCheeseViewModel(IEnumerable<CheeseCategory> categories)
+
+        {
+
+
+
+            Categories = new List<SelectListItem>();
+
+
+
+            foreach (var category in categories)
+
             {
-                Value = ((int)CheeseType.Soft).ToString(),
-                Text = CheeseType.Soft.ToString()
-            });
 
-            CheeseTypes.Add(new SelectListItem
-            {
-                Value = ((int)CheeseType.Fake).ToString(),
-                Text = CheeseType.Fake.ToString()
-            });
+                Categories.Add(new SelectListItem
+
+                {
+
+                    Value = category.ID.ToString(),
+
+                    Text = category.Name
+
+                });
+
+            }
+
+
+
+
+
+            //Categories = new List<SelectListItem>();
+
+
+
+            //Categories.Add(new SelectListItem
+
+            //{
+
+            //    Value = CategoryID.ToString(),
+
+            //    Text = Name
+
+            //});
+
+
+
+            //CheeseTypes = new List<SelectListItem>()
+
+            //{
+
+            //    Value = Categories.
+
+            //}
+
+
+
+
+
+            //CheeseTypes = new List<SelectListItem>();
+
+
+
+            //// <option value="0">Hard</option>
+
+            //CheeseTypes.Add(new SelectListItem {
+
+            //    Value = ((int) CheeseType.Hard).ToString(),
+
+            //    Text = CheeseType.Hard.ToString()
+
+            //});
+
+
+
+            //CheeseTypes.Add(new SelectListItem
+
+            //{
+
+            //    Value = ((int)CheeseType.Soft).ToString(),
+
+            //    Text = CheeseType.Soft.ToString()
+
+            //});
+
+
+
+            //CheeseTypes.Add(new SelectListItem
+
+            //{
+
+            //    Value = ((int)CheeseType.Fake).ToString(),
+
+            //    Text = CheeseType.Fake.ToString()
+
+            //});
+
+
 
         }
+
     }
+
 }
---
+++ src/CheeseMVC/ViewModels/AddMenuItemViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
+using System.Linq;
+
+using System.Threading.Tasks;
+
+using CheeseMVC.Models;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+
+namespace CheeseMVC.ViewModels
+
+{
+
+    public class AddMenuItemViewModel
+
+    {
+
+        // capitalizing c C and m M
+
+        [Required]
+
+        [Display(Name = "Cheese")]
+
+        public int CheeseID { get; set; }
+
+        public int MenuID { get; set; }
+
+
+
+        public Menu Menu { get; set; }
+
+        public List<SelectListItem> Cheeses { get; set; }
+
+
+
+        public AddMenuItemViewModel() { }
+
+
+
+        public AddMenuItemViewModel(Menu aMenu, IEnumerable<Cheese> theSelectListCheeses)
+
+        {
+
+            Menu = aMenu;
+
+
+
+            Cheeses = new List<SelectListItem>();
+
+
+
+            foreach (var cheese in theSelectListCheeses)
+
+            {
+
+                Cheeses.Add(new SelectListItem
+
+                {
+
+                    Value = cheese.ID.ToString(),
+
+                    Text = cheese.Name
+
+                });
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+}
